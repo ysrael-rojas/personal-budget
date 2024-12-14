507bc519daf4f7562765b6d642ef6339a3fb4e11
ysrael-rojas <yrra.rojas@gmail.com>
2024-12-14 07:34:00 -0500

    Refactorizando primera parte

--- js/Budget.js
+++ js/Budget.js
@@ -3,41 +3,23 @@ function Budget() {
     this.transactions = [];
 }
 
-Budget.prototype.add = (transaction) => {
+Budget.prototype.add = function(transaction) {
 
     this.transactions.push(transaction);
 }
 
-Budget.prototype.remove = (id) => {
+Budget.prototype.remove = function(id) {
 
     this.transactions = this.transactions.filter( (transaction) => {
         return transaction.id !== id;
     } );
 }
 
-Budget.prototype.calculateTotal = () => {
+Budget.prototype.calculateBalance = function() {
 
+    return this.transactions.reduce( (total, transaction) => {
 
-}
-
-Budget.prototype.calculateBalance = () => {
-
-    return this.transacciones.reduce( (total,{ amount }) => {
-
-        return total + amount;
+        return total + transaction.getSignedAmount();
 
     }, 0);
-}
-
-Budget.prototype.formatAmount = () => {
-
-     // Utilizamos el objeto Intl.NumberFormat para formatear el número
-     const formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-    });
-    
-    return formatter.format(this.amount);    
 }
--- js/Transaction.js
+++ js/Transaction.js
@@ -1,24 +1,37 @@
-function Transaction(type, amount, description) {
+function Transaction(type, amount) {
 
     this.id = Date.now();
     this.type = type;
-    this.description = description;
     this.amount = amount;
     this.createdDate = new Date();
 }
 
-Transaction.prototype.getFormattedDate = () => {
-    // Retorna la fecha como String en formato legible
-    const dia = String(this.createdDate.getDate()).padStart(2, '0'); // Aseguramos que el día tenga 2 dígitos
-    const mes = String(this.createdDate.getMonth() + 1).padStart(2, '0'); // Los meses van de 0 a 11, así que sumamos 1
-    const año = this.createdDate.getFullYear(); // Obtener el año completo
-
-    // Formato de fecha: día/mes/año
-    return `${dia}/${mes}/${año}`;
+Transaction.prototype.getFormattedDate = function() {
+    
+    return this.createdDate.toLocaleDateString();
 };
 
-Transaction.prototype.getSignedAmount = () => {
+Transaction.prototype.getSignedAmount = function() {
     
     return this.type === '1' ? this.amount : -this.amount;
 
 };
+
+Transaction.prototype.formatAmount = function(amount) {
+
+    // Utilizamos el objeto Intl.NumberFormat para formatear el número
+    const formatter = new Intl.NumberFormat('en-US', {
+       style: 'currency',
+       currency: 'USD',
+       minimumFractionDigits: 2,
+       maximumFractionDigits: 2
+   });
+   
+   return formatter.format(amount);    
+}
+
+Transaction.prototype.getFormattedSignedAmount = function() {
+    
+    const signedAmount = this.getSignedAmount();
+    return this.formatAmount(signedAmount);
+}
